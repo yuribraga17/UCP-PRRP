6684eec2910f78300eec3fc35adb5e7f14b5a381
Yuri Braga <yuribragasoares@gmail.com>
2022-10-29 12:01:32 -0300

    att icones

--- menu.php
+++ menu.php
@@ -35,16 +35,24 @@ if($link == '/ucp/inicio') echo '<li class="active">'; else echo "<li>";
 	<a href="<?=$url_base;?>/inicio"><i class="fa fa-user"></i> <span class="nav-label">Meus Personagens</span></a>
 
 </li>
+<?php
+if($link == '/ucp/criar') echo '<li class="active">'; else echo "<li>"; 
+?>
+	<a href="<?=$url_base;?>/criar"><i class="fa fa-plus-square-o"></i> <span class="nav-label">Criar Personagem</span></a>
+</li>
+
+<li>
 <?php 
 if($link == '/ucp/account') echo '<li class="active">'; else echo "<li>";
 ?>
 <hr style="border: 1px solid <?=$cor_linha_menu;?>; margin-top:2px; margin-bottom:2px;">
 	<a href="<?=$url_base;?>/account"><i class="fa fa-gears"></i> <span class="nav-label">Painel de Controle</span></a>
 
+
 <?php
-if($link == '/ucp/criar') echo '<li class="active">'; else echo "<li>"; 
+if($link == '/ucp/online') echo '<li class="active">'; else echo "<li>"; 
 ?>
-	<a href="<?=$url_base;?>/criar"><i class="fa fa-plus-square-o"></i> <span class="nav-label">Criar Personagem</span></a>
+	<a href="<?=$url_base;?>/online"><i class="fa fa-id-badge"></i> <span class="nav-label">Total online</span></a>
 </li>
 
 <!-- ÁREA DE ADMINISTRADORES-->
@@ -64,6 +72,7 @@ if($link == '/ucp/criar') echo '<li class="active">'; else echo "<li>";
 	<a href="<?=$url_base;?>/log5"><i class="fa fa-cog"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Log kick's</span></a>
 	<a href="<?=$url_base;?>/log6"><i class="fa fa-cog"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Log ajail</span></a>
 	<a href="<?=$url_base;?>/log7"><i class="fa fa-cog"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Log banimento</span></a>
+	<a href="<?=$url_base;?>/grafico"><i class="fa fa-cog"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Gráfico</span></a>
 	</ul>
  </li>
  <?php } ?>
@@ -80,10 +89,10 @@ if($link == '/ucp/criar') echo '<li class="active">'; else echo "<li>";
 			<ul class="nav nav-second-level collapse">';
 	if($link == '/ucp/apps') echo '<li class="active">'; else echo "<li>"; 
  ?>
-	<a href="<?=$url_base;?>/log4"><i class="fa fa-cog"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Log armas</span></a>
-	<a href="<?=$url_base;?>/log"><i class="fa fa-cog"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Refundo arma</span></a>
-	<a href="<?=$url_base;?>/log2"><i class="fa fa-cog"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Refundo dinheiro</span></a>
-	<a href="<?=$url_base;?>/log3"><i class="fa fa-cog"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Refundo itens</span></a></li>
+	<a href="<?=$url_base;?>/log4"><i class="fa fa-archive"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Log armas</span></a>
+	<a href="<?=$url_base;?>/log"><i class="fa fa-archive"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Refundo arma</span></a>
+	<a href="<?=$url_base;?>/log2"><i class="fa fa-archive"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Refundo dinheiro</span></a>
+	<a href="<?=$url_base;?>/log3"><i class="fa fa-archive"></i><i class="fa  fa-sort-alpha-down"></i> <span class="nav-label">Refundo itens</span></a></li>
 
 	</ul>
  </li>
@@ -101,12 +110,12 @@ if($link == '/ucp/criar') echo '<li class="active">'; else echo "<li>";
 		echo '<li class="active">'; else echo "<li>"; 
 		
 	echo '
-			<a href="#"><i class="fa fa-flag"></i> <span class="nav-label">Area policial</span><span class="fa arrow"></span></a>
+			<a href="#"><i class="fa fa-desktop"></i> <span class="nav-label">Area policial</span><span class="fa arrow"></span></a>
 			<ul class="nav nav-second-level collapse">';
 	if($link == 'www.google.com') echo '<li class="active">'; else echo "<li>"; 
 ?>
-	<a href="<?=$url_base;?>/190"><i class="fa fa-cog"></i><i class="fa  fa-angle-right"></i> <span class="nav-label">registro 190</span></a>
-	<a href="<?=$url_base;?>/criminal"><i class="fa fa-cog"></i><i class="fa  fa-angle-right"></i> <span class="nav-label">Fichas Criminais</span></a>
+	<a href="<?=$url_base;?>/190"><i class="fa fa-archive"></i><i class="fa  fa-angle-right"></i> <span class="nav-label">registro 190</span></a>
+	<a href="<?=$url_base;?>/criminal"><i class="fa fa-archive"></i><i class="fa  fa-angle-right"></i> <span class="nav-label">Fichas Criminais</span></a>
 	</ul></li></li>
 </li>
 <!-- Start Footer Section -->
